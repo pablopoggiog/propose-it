ab5033fdcd8002d8722c5dd932983224c6780ea7
Pablo Poggio <pablopoggiog@gmail.com>
2022-08-06 19:07:27 -0300

    Set up routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-const App = () => {
-  return <div className="App">I'm the app</div>;
-};
+import { VStack } from "@chakra-ui/react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header } from "src/components";
 
-export default App;
+export const App = () => (
+  <VStack>
+    <Header />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div />}>
+          <Route index element={<div />} />
+          {/* <Route path="proposal/:id" element={<ProposalPage />}></Route> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </VStack>
+);
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
-import App from "./App";
+import { App } from "./App";
+import { theme } from "src/theme";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
@@ -10,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
