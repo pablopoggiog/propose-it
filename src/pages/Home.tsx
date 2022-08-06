10bae75a412892a2c2fe7b76d96e51ee75da4b70
Pablo Poggio <pablopoggiog@gmail.com>
2022-08-06 19:17:26 -0300

    Add 3D model as background in homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,8 +7,26 @@ import {
   TabPanel,
   Flex
 } from "@chakra-ui/react";
+import Spline from "@splinetool/react-spline";
+import styled from "@emotion/styled";
+import { keyframes } from "@emotion/react";
 import { Topic } from "src/components/Topic";
 import { topics } from "src/mocks";
+
+const bounce = keyframes`
+  from {
+      opacity: 0;
+      transform: translateX(10px);
+    }
+    to {
+      opacity: 1;
+      transform: translateX(0);
+    }
+    `;
+
+const AnimatedSpline = styled(Spline)`
+  animation: ${bounce} 3s ease-in-out;
+`;
 
 export const HomePage: FC = () => {
   return (
@@ -21,6 +39,22 @@ export const HomePage: FC = () => {
       pos="relative"
       bg="#f0acfc"
     >
+      <Flex
+        w="full"
+        pos="absolute"
+        top={0}
+        left={0}
+        bottom={0}
+        right={0}
+        zIndex={0}
+        opacity={0.5}
+      >
+        <AnimatedSpline
+          id="canva"
+          scene="https://prod.spline.design/qzyFOMbsVr6Wj3E9/scene.splinecode"
+          width="100vw"
+        />
+      </Flex>
       <Tabs colorScheme="teal" w="full" zIndex={1} bg="primary" opacity={0.9}>
         <TabList w="full" justifyContent="space-between">
           {topics.map(({ id, name }) => (
