f09df4ae4e27de8057fde42db42afc4ff9d86172
Pablo Poggio <pablopoggiog@gmail.com>
2022-08-06 19:15:01 -0300

    Start displaying topics and proposals in homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,14 @@
 import { FC } from "react";
-import { Flex } from "@chakra-ui/react";
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Flex
+} from "@chakra-ui/react";
+import { Topic } from "src/components/Topic";
+import { topics } from "src/mocks";
 
 export const HomePage: FC = () => {
   return (
@@ -12,7 +21,23 @@ export const HomePage: FC = () => {
       pos="relative"
       bg="#f0acfc"
     >
-      Homepage
+      <Tabs colorScheme="teal" w="full" zIndex={1} bg="primary" opacity={0.9}>
+        <TabList w="full" justifyContent="space-between">
+          {topics.map(({ id, name }) => (
+            <Tab key={id} w="full">
+              {name}
+            </Tab>
+          ))}
+        </TabList>
+
+        <TabPanels>
+          {topics.map((topic) => (
+            <TabPanel>
+              <Topic key={topic.id} {...topic} />
+            </TabPanel>
+          ))}
+        </TabPanels>
+      </Tabs>
     </Flex>
   );
 };
