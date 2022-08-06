1ff12dc1c456bc56487763a013b21053ee906b02
Pablo Poggio <pablopoggiog@gmail.com>
2022-08-06 18:44:59 -0300

    Mock proposals data

---
+++ src/mocks/index.ts
@@ -0,0 +1,32 @@
+const proposals = [
+  {
+    id: 1,
+    name: "Proposal 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  },
+  {
+    id: 2,
+    name: "Proposal 2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+  }
+];
+
+export const topics = [
+  {
+    id: 1,
+    name: "APR for 2023",
+    proposals: proposals
+  },
+  {
+    id: 2,
+    name: "New allowances",
+    proposals: proposals
+  },
+  {
+    id: 3,
+    name: "Budget for research 2023",
+    proposals: proposals
+  }
+];
